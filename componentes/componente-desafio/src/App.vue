166227e5f53eeaa165f8b4bd6212aaddd1216e48
SamRFreitas <samuelrfreitassi@gmail.com>
2019-11-26 17:57:32 -0300

    Desafio do primeiro Módulo de Componentes concluído

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+	<v-app>
+		<!-- Menu -->
+		<app-navigation/>
+		
+		<!-- Conteúdo -->
+		<app-toolbar/>
+		<v-content>
+			<app-carousel/>
+		</v-content>
+		
+		<!-- Rodapé -->
+		<app-footer/>
+	</v-app>
+</template>
+
+<script>
+
+import Navigation from './Navigation.vue'
+import Footer from './Footer.vue'
+import Carousel from './Carousel.vue'
+import Toolbar from './Toolbar'
+
+export default {
+	components:{
+		'app-navigation': Navigation,
+		'app-footer': Footer,
+		'app-carousel': Carousel,
+		'app-toolbar': Toolbar
+	}
+}
+
+</script>
