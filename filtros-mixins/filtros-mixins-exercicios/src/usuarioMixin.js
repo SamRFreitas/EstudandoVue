7e68e9a572562327d2dc4483b53e819a4998c2f2
SamRFreitas <samuelrfreitassi@gmail.com>
2020-01-31 12:04:30 -0300

    Módulo de Animações concluído

---
+++ filtros-mixins/filtros-mixins-exercicios/src/usuarioMixin.js
@@ -0,0 +1,10 @@
+export default {
+    computed:{
+        usuarioLogado(){
+            return 'Samuel Ribeiro'
+        }
+    },
+    created(){
+        console.log('Created - usuarioMixin.js!')
+    }
+}
